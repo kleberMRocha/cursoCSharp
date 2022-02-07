6250eb81c5135777f37adf6329638ac16de6ac57
kleber <kleberrocha2701@gmail.com>
2022-02-06 22:36:24 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cursocsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "Hello world";
+            Console.WriteLine(hello);
+        }
+    }
+}
